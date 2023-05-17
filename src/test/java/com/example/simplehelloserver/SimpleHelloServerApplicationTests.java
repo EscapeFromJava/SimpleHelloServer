e006c8591e52bd6a1794ba8b3c7b7fae783a83e2
EscapeFromJava <reske.artur@gmail.com>
2023-05-17 13:33:17 +0300

    init

---
+++ src/test/java/com/example/simplehelloserver/SimpleHelloServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplehelloserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHelloServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
